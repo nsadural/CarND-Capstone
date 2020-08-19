c623593d1ec837666c2bd3ea320b3967106786c7
nsadural <nsadural@purdue.edu>
2020-08-18 23:19:50 -0700

    Update control with MPC, working on debug

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -3,7 +3,8 @@
 import rospy
 from std_msgs.msg import Bool
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, PoseStamped
+from styx_msgs.msg import Lane, Waypoint
 import math
 #from mpc import MPC    # NOTE: Python 3.X package, does not work with Python 2.7
 
