0644933ad70de3c9f34d2ac899764587c3109bbb
nsadural <nsadural@purdue.edu>
2020-10-20 00:52:35 +0000

    Tuning LQR, mean cte: -1.4 cm, rms cte: 31.4 cm

--- ros/src/twist_controller/lateral_lqr.py
+++ ros/src/twist_controller/lateral_lqr.py
@@ -11,7 +11,12 @@ class LateralLQR(object):
         self.steer_ratio = steer_ratio
         
         self.sample_time = 0.020
-        self.control_horizon = 9
+        self.control_horizon = 5
+        self.n_rmse = 0
+        self.running_cte = 0.0
+        self.running_sq_cte = 0.0
+        self.mean_cte = 0.0
+        self.rmse = 0.0
 
     def get_steering(self, current_x, current_y, current_psi, current_velocity, current_yaw_rate, trajectory_x, trajectory_y, trajectory_psi, des_velocity, des_yaw_rate):
         # Translate vehicle and trajectory points to trajectory frame
@@ -39,8 +44,17 @@ class LateralLQR(object):
 
         # Current vehicle error state
         cte = (cs(0, nu=1)*x - y + trajectory_y[0]) / (np.sqrt(pow(cs(0, nu=1), 2) + 1))
-        #epsi = (psi - np.arctan(cs(5, nu=1)))
         epsi = psi - trajectory_psi[0]
+        
+        # Calculate root mean square crosstrack error
+        self.n_rmse += 1
+        self.running_cte += cte
+        self.running_sq_cte += cte**2
+        self.mean_cte = self.running_cte/self.n_rmse
+        self.rmse = np.sqrt(self.running_sq_cte/self.n_rmse)
+        
+        rospy.logwarn("Mean crosstrack error [m]: {0}".format(self.mean_cte))
+        rospy.logwarn("Root mean square crosstrack error [m]: {0}\n".format(self.rmse))
 
         ### LQR gain schedule
         k1 = -0.000077*current_velocity + 0.044710
@@ -48,7 +62,7 @@ class LateralLQR(object):
 
         # Feedforward compensation
         steering_ff = np.arctan(des_yaw_rate*self.wheel_base/des_velocity)
-        k_ff = 1.5
+        k_ff = 1.2
         
         # LQR with feedforward compensation
         steering = (-k1*cte - k2*epsi + k_ff*steering_ff)*self.steer_ratio
@@ -58,9 +72,10 @@ class LateralLQR(object):
             steering = self.max_steer
         elif steering < self.min_steer:
             steering = self.min_steer
-            
-        rospy.logwarn("cte [m]: {0}".format(cte))
-        rospy.logwarn("epsi [rad]: {0}".format(epsi))
+        
+        ### DEBUG ###
+        #rospy.logwarn("cte [m]: {0}".format(cte))
+        #rospy.logwarn("epsi [rad]: {0}".format(epsi))
         #rospy.logwarn("x [m]: {0}".format(x))
         #rospy.logwarn("y [m]: {0}".format(y))
         #rospy.logwarn("trajectory_x[0]: {0}".format(trajectory_x[0]))
@@ -68,7 +83,7 @@ class LateralLQR(object):
         #rospy.logwarn("trajectory_x[1]: {0}".format(trajectory_x[1]))
         #rospy.logwarn("trajectory_y[1]: {0}".format(trajectory_y[1]))
         #rospy.logwarn("steering_ff [rad]: {0}".format(steering_ff))
-        rospy.logwarn("delta [rad]: {0}\n".format(steering))
+        #rospy.logwarn("delta [rad]: {0}\n".format(steering))
         
         steering_list = []
         steering_list.append(steering)
@@ -90,4 +105,3 @@ class LateralLQR(object):
             steering_next = self.min_steer
             
         return steering_next, cte_next, epsi_next
-    
